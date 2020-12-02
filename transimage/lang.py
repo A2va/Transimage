416f18172ec9845d1ceb7de06fa80f92305e945a
A2va <49582555+A2va@users.noreply.github.com>
2020-12-02 15:58:34 +0100

    Add all langs

---
+++ transimage/lang.py
@@ -0,0 +1,143 @@
+ALL_LANG={
+            
+    #'abq':'abaza',                     #Abaza
+    #'ady':'adyghe',                    #Adyghe
+    #'anp':'angika',                    #Angika
+    'afr': 'afrikaans',                 #Afrikaans
+    'amh': 'amharic',                   #Amharic
+    'ara': 'arabic',                    #Arabic
+    #'asm': 'assamese',                 #Assamese
+    #'ava':'avar',                      #Avar
+    'aze': 'azerbaijani',               #Azerbaijani
+    'aze_cyrl': 'azerbaijani cyrillic', #Azerbaijani (cyrillic) 
+    #'bih': 'bihari',                   #Bihari      
+    'bel': 'belarusian',                #Belarusian
+    'ben': 'bengali',                   #Bengali
+    #'bho': 'bhojpuri',                 #Bhojpuri
+    #'bod': 'tibetan',                  #Tibetan
+    'bos': 'bosnian' ,                  #Bosnian
+    'bul': 'bulgarian',                 #Bulgarian
+    'cat': 'catalan',                   #Catalan
+    'ceb': 'cebuano',                   #Cebuano 
+    'ces': 'czech',                     #Czech
+    #'che': 'chechen',                  #Chechen
+    'chi_sim': 'chinese (simplified)',                  #Chinese (simplified)
+    'chi_sim_vert': 'chinese vertical (simplified)',    #Chinese vertical (simplified)
+    'chi_tra':'chinese (traditional)',                  #Chinese (traditional)
+    'chi_tra_vert': 'chinese vertical (traditionnal)',  #Chinese vertical (traditional) 
+    #'chr': 'cherokee',                     #Cherokee
+    'cos': 'corsican',                      #Corsican
+    'cym': 'welsh',                         #Welsh
+    #'sim': 'mende',                        #Mende (Papua New Guinea)
+    'dan': 'danish',                        #Danish
+    #'dar': 'dargwa,                        #Dargwa
+    'deu': 'german',                        #German
+    #'div': 'divehi,                        #Divehi
+    #'dzo': dzongkha,                       #Dzongkha
+    'ell': 'greek',                         #Greek (modern)
+    'eng': 'english',                       #English
+    #'enm': 'english (old)',                #English (middle)
+    'est': 'estonian',                      #Estonian
+    'eus': 'basque',                        #Basque
+    #'fao': 'fao',                          #Faroese
+    'fas': 'persian',                       #Persian
+    'fin': 'finnish',                       #Finnish
+    'fra': 'french',                        #French
+    'frk': 'frankish',                      #Frankish 
+    'frm': 'french (middle)',               #French (middle)
+    'fry': 'frisian',                       #Frisian (western)
+    'gla': 'scottish gaelic',               #Scottish Gaelic
+    'gle': 'irish',                         #Irish
+    'glg': 'galician',                      #Galician
+    #'gom': 'goan konkani',                 #Goan Konkani
+    #'grc': 'greek (ancient)',              #Greek (ancient)
+    #'quj': 'gujuarati,                     #Gujuarati
+    'hat': 'haitian',                       #Haitian
+    'heb': 'hebrew',                        #Hebrew
+    'hin': 'hindi',                         #Hindi
+    'hrv': 'croatian',                      #Croatian
+    'hun': 'hungarian',                     #Hungarian
+    'hye': 'armenian',                      #Armenian
+    #'iku': 'inuktitut',                    #Inuktitut 
+    'ind': 'indonesian',                    #Indonesian
+    #'inh': 'ingush'                        #Ingush
+    'isl': 'icelandic',                     #Icelandic
+    'ita': 'italian',                       #Italian
+    #'ita_old': 'italian (old)',            #Italian (old)
+    'jav': 'javanese',                      #Javanese
+    'jpn': 'japanese',                      #Japanese
+    'jpn_vert': 'japanese (vertical)',      #Japanese (vertical)
+    'kan': 'kannada',                       #Kannada
+    'kat': 'georgian',                      #Georgian
+    #'kat_old': 'georgian (old)',           #Georgian (old)
+    'kaz': 'kazakh',                        #Kazakh
+    #'kdb': 'kavardian',                    #Kabardian
+    'khm': 'khmer',                         #Khmer
+    #'kir': 'kirghz',                       #Kirghiz 
+    'kmr': 'kurdish',                       #Kurdish (northerm)
+    'kor': 'korean',                        #Korean
+    'kor_vert': 'korean (vertical)',        #Korean (vertical)
+    'lao': 'lao',                           #Lao
+    'lat': 'latin',                         #Latin
+    'lav': 'latvian',                       #Latvian
+    #'lbe': 'lak',                          #Lak
+    #'lez': 'lezghian',                     #Lezghian
+    'lit': 'lithuanian',                    #Lithuanian 
+    'ltz': 'luxembourgish',                 #Luxembourgish 
+    #'mai': 'maithili',                     #Maithili
+    #'mah': 'magahi',                       #Magahi
+    'mal': 'malayalem',                     #Malayalam
+    'mar': 'marathi',                       #Marathi 
+    'mkd': 'macedonian',                    #Macedonian 
+    'mlt': 'maltese',                       #Maltese 
+    'mon': 'mongalian',                     #Mongolian 
+    'mri': 'maori',                         #Maori 
+    'msa': 'malay',                         #Malay 
+    'mya': 'burmese',                       #Burmese 
+    'nep': 'nepali',                        #Nepali 
+    'new': 'newari',                        #Newari
+    'nld': 'dutch',                         #Dutch
+    'nor': 'norwegian',                     #Norwegian
+    #'oci': 'occitan',                      #Occitan 
+    'ori': 'oriya',                         #Oriya 
+    #'pan': 'panjabi',                      #Panjabi 
+    'pol': 'polish',                        #Polish 
+    'por': 'portuguese',                    #Portuguese 
+    'pus': 'pushto',                        #Pushto 
+    #'que': 'quechua',                      #Quechua 
+    'ron': 'romanian',                      #Romanian 
+    'rus': 'russian',                       #Russian
+    #'san': 'sanskrit',                     #Sanskrit
+    #'sck': 'nagpuri',                      #Nagpuri
+    'sin': 'sinhala',                       #Sinhala 
+    'slk': 'slovak',                        #Slovak 
+    'slv': 'slovenian',                     #Slovenian 
+    'snd': 'sindhi',                        #Sindhi 
+    'spa': 'spanish',                       #Spanish 
+    #'spa_old': 'spanish (old)',            #Spanish (old)
+    'sqi': 'albanian',                      #Albanian 
+    'srp': 'serbian (cyrillic)',            #Serbian (cyrillic)
+    'srp_latn': 'serbian (latin)',          #Serbian (latin)
+    'sun': 'sundanese',                     #Sundanese 
+    'swa': 'swahili',                       #Swahili 
+    'swe': 'swedish',                       #Swedish
+    #'syr': 'syriac',                       #Syriac
+    #'tab': 'tabassaran',                   #Tabassaran
+    'tam': 'tamil',                         #Tamil
+    'tat': 'tatar',                         #Tatar
+    'tel': 'telegu',                        #Telugu
+    'tgk': 'taijiik',                       #Tajiik
+    'tha': 'thai',                          #Thai 
+    #'tir': 'tigrinya',                     #Tigrinya
+    #'tl': 'tagalog',                       #Tagalog 
+    'ton': 'tonga',                         #Tonga 
+    'tur': 'turkish',                       #Turkish 
+    'uig': 'uighur',                        #Uighur 
+    'ukr': 'ukrainian',                     #Ukrainian 
+    'urd': 'urdu',                          #Urdu
+    'uzb': 'uzbek',                         #Uzbek
+    'urb_cyrl': 'uzbek (cyrillic)',         #Uzbek (cyrillic)
+    'vie': 'vietnamese',                    #Vietnamese 
+    'yid': 'yiddish',                       #Yiddish 
+    'yor': 'yoruba',                        #Yoruba 
+}
