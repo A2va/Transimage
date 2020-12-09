d1699c6859429f89fc04ebc255cf690940bc4606
A2va <49582555+A2va@users.noreply.github.com>
2020-12-09 12:17:01 +0100

    Add license header

--- transimage/canvas.py
+++ transimage/canvas.py
@@ -1,3 +1,18 @@
+# This file is part of Transimage.
+
+# Transimage is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Transimage is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Transimage. If not, see <https://www.gnu.org/licenses/>.
+
 import wx
 import cv2
 import numpy as np
